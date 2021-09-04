2bd4261ff11d87c53a8e9f3c007ea714048992a4
Bogdan-K-A <kozhoma_b@ukr.net>
2021-09-04 13:51:58 +0300

    auto-test.js

--- js/test-01.js
+++ js/test-01.js
@@ -86,28 +86,169 @@
 //   return isNotInRange
 // }
 /* ----------------------------------- 1-24 ----------------------------------- */
-// Ращёт скидки от и до
-function getDiscount(totalSpent) {
-  const BASE_DISCOUNT = 0
-  const BRONZE_DISCOUNT = 0.02
-  const SILVER_DISCOUNT = 0.05
-  const GOLD_DISCOUNT = 0.1
-  let discount
+// Раcщёт скидки от и до
+// function getDiscount(totalSpent) {
+//   const BASE_DISCOUNT = 0
+//   const BRONZE_DISCOUNT = 0.02
+//   const SILVER_DISCOUNT = 0.05
+//   const GOLD_DISCOUNT = 0.1
+//   let discount
 
-  if (totalSpent >= 50000) {
-    discount = GOLD_DISCOUNT
-  } else if (totalSpent >= 20000 && totalSpent < 50000) {
-    discount = SILVER_DISCOUNT
-  } else if (totalSpent >= 5000 && totalSpent < 20000) {
-    discount = BRONZE_DISCOUNT
-  } else {
-    discount = BASE_DISCOUNT
-  }
+//   if (totalSpent >= 50000) {
+//     discount = GOLD_DISCOUNT
+//   } else if (totalSpent >= 20000 && totalSpent < 50000) {
+//     discount = SILVER_DISCOUNT
+//   } else if (totalSpent >= 5000 && totalSpent < 20000) {
+//     discount = BRONZE_DISCOUNT
+//   } else {
+//     discount = BASE_DISCOUNT
+//   }
 
-  return discount
-}
-/* ----------------------------------- 1-06 ----------------------------------- */
-/* ----------------------------------- 1-06 ----------------------------------- */
-/* ----------------------------------- 1-06 ----------------------------------- */
-/* ----------------------------------- 1-06 ----------------------------------- */
-/* ----------------------------------- 1-06 ----------------------------------- */
+//   return discount
+// }
+/* ----------------------------------- 1-25 ----------------------------------- */
+// Тернарный оператор
+// function checkStorage(available, ordered) {
+//   let message
+
+//   message =
+//     ordered > available
+//       ? 'Not enough goods in stock!'
+//       : 'The order is accepted, our manager will contact you'
+
+//   return message
+// }
+/* ----------------------------------- 1-27 ----------------------------------- */
+// Инструкция switch
+// function getSubscriptionPrice(type) {
+//   let price
+
+//   switch (
+//     type // Change this line
+//   ) {
+//     case 'starter': // Change this line
+//       price = 0 // Change this line
+//       break
+
+//     case 'professional': // Change this line
+//       price = 20 // Change this line
+//       break
+
+//     case 'organization': // Change this line
+//       price = 50 // Change this line
+//       break
+//   }
+
+//   return price
+// }
+/* ----------------------------------- 1-28 ----------------------------------- */
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = 'jqueryismyjam'
+//   let message
+
+//   switch (password) {
+//     case null:
+//       message = 'Canceled by user!'
+//       break
+
+//     case ADMIN_PASSWORD:
+//       message = 'Welcome!'
+//       break
+
+//     default:
+//       message = 'Access denied, wrong password!'
+//   }
+
+//   return message
+// }
+/* ----------------------------------- 1-29 ----------------------------------- */
+// function getShippingCost(country) {
+//   let message;
+
+// switch (country) {
+//     case"Australia":
+//     price = 170
+//       message = `Shipping to ${country} will cost ${price} credits`
+//       break
+
+//     case"Germany":
+//        message = "Sorry, there is no delivery to your country"
+//       break
+
+//     case"China":
+//     price = 100
+//       message = `Shipping to ${country} will cost ${price} credits`
+//       break
+
+//     case"Chile":
+//     price = 250
+//       message = `Shipping to ${country} will cost ${price} credits`
+//       break
+
+//      case"Jamaica":
+//     price = 120
+//       message = `Shipping to ${country} will cost ${price} credits`
+//       break
+
+//      case"Sweden":
+//        message = "Sorry, there is no delivery to your country"
+
+//   }
+
+//   return message;
+// }
+/* ----------------------------------- 1-30 ----------------------------------- */
+// длинна строки
+// function getNameLength(name) {
+//   const message = `Name ${name} is ${name.length} characters long`;
+//   return message;
+// }
+/* ----------------------------------- 1-31 ----------------------------------- */
+// Индексация элементов строки
+// const courseTopic = "JavaScript essentials";
+
+// const courseTopicLength = courseTopic.length;// вся длнинна значения
+// const firstElement = courseTopic[0];//первый элемент
+// const lastElement = courseTopic[courseTopic.length -1];//последний элемент
+/* ----------------------------------- 1-32 ----------------------------------- */
+// Метод slice()
+// function getSubstring(string, length) {
+//   const substring = string.slice(0, length);
+
+//   return substring;
+// }
+/* ----------------------------------- 1-33 ----------------------------------- */
+
+// function formatMessage(message, maxLength) {
+//   let result
+
+//   if (message.length === maxLength) {
+//     return message
+//   }
+//   message.length > maxLength
+//   result = message.slice(0, maxLength) + '...'
+
+//   return result
+// }
+/* ----------------------------------- 1-34 ----------------------------------- */
+// Методы toLowerCase() и toUpperCase()
+// function normalizeInput(input) {
+//   const normalizedInput = input.toLowerCase();
+
+//   return normalizedInput;
+// }
+/* ----------------------------------- 1-35 ----------------------------------- */
+// function checkForName(fullName, name) {
+//   const result = fullName.includes(name)
+//   return result
+// }
+/* ----------------------------------- 1-36 ----------------------------------- */
+// Задача: проверка спама (нужно все буквы при поиске приводить в один регистр)
+// function checkForSpam(message) {
+//   let result;
+
+//  const lowerCase = message.toLowerCase()
+// result = lowerCase.includes('spam') || lowerCase.includes('sale')  ? true :  false
+
+//   return result;
+// }
